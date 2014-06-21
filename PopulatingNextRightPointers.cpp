b877c3b9ab03bda62075b1c1069f1c5db3919b76
wgx-precy <wgx.nuaa@gmail.com>
2014-06-21 15:58:27 -0400

    Populating Next Right Pointers

---
+++ PopulatingNextRightPointers.cpp
@@ -0,0 +1,20 @@
+//Populating Next Right Pointers in Each Node
+/*Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL*/
+//Sat 3:56 Jun21
+void connect(TreeLinkNode *root) {
+    if(!root) return;
+    TreeLinkNode *tmp = root;
+    TreeLinkNode *node;
+    TreeLinkNode *next_sib = NUll;
+    while(tmp->left) {
+	node = tmp;
+	while(tmp->left) {
+	    next_sib = node->next->left;
+	    node->left->next = node->right;
+	    node->right->next = next_sib;
+	    node = node->next;
+	}
+	node->left->next = node->right;
+	tmp = tmp->left;
+    }
+
